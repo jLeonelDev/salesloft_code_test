0f62f589700582d5991d081ecde83955ff3fbd1c
Leonel Adame <jleoneldev@MacBook-Pro-de-Leonel.local>
2021-01-25 17:58:25 -0600

    mix fix and .sh commands created

--- backend/mix.exs
+++ backend/mix.exs
@@ -43,4 +43,16 @@ defmodule SalesLoft.MixProject do
       {:httpoison, "~> 1.8"}
     ]
   end
+
+  # Aliases are shortcuts or tasks specific to the current project.
+  # For example, to install project dependencies and perform other setup tasks, run:
+  #
+  #     $ mix setup
+  #
+  # See the documentation for `Mix` for more info on aliases.
+  defp aliases do
+    [
+      test: ["test"]
+    ]
+  end
 end
---
+++ test.backend.sh
@@ -0,0 +1,1 @@
+docker-compose exec salesloft_backend /bin/sh -c "MIX_ENV=test mix test"
