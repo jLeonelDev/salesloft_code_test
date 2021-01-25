8695d4b0026b88fe2e818ba20e8f71b5a04f099f
Leonel Adame <jleoneldev@MacBook-Pro-de-Leonel.local>
2021-01-25 17:33:21 -0600

    fallback controller @doc added and enhance

--- backend/lib/sales_loft_api/controllers/fallback_controller.ex
+++ backend/lib/sales_loft_api/controllers/fallback_controller.ex
@@ -1,21 +1,28 @@
 defmodule SalesLoftApi.FallbackController do
+  @moduledoc """
+  Process Controller response to avoid
+  code duplication.
+  """
   use Plug.Builder
 
-  @spec call(map, {:success, map | list}) :: map
+  @doc """
+  Handles Status Code Responses
+  """
+  @spec call(Plug.Conn.t(), {:success, map | list}) :: map
   def call(conn, {:success, body}) do
     conn
     |> put_resp_content_type("application/json")
     |> send_resp(200, Jason.encode!(body))
   end
 
-  @spec call(map, {:created, map | list}) :: map
+  @spec call(Plug.Conn.t(), {:created, map | list}) :: map
   def call(conn, {:created, body}) do
     conn
     |> put_resp_content_type("application/json")
     |> send_resp(201, Jason.encode!(body))
   end
 
-  @spec call(map, {:error, String.t()}) :: map
+  @spec call(Plug.Conn.t(), {:error, String.t()}) :: map
   def call(conn, {:error, message}) when is_binary(message) do
     conn
     |> put_resp_content_type("application/json")
